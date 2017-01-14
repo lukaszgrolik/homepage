5f4ca75868f209b26bf934e8d2117fbc8e968c96
Łukasz Grolik <lukasz@grolik.pl>
2017-01-14 15:17:55 +0100

    Init branch

--- src/app/index.js
+++ src/app/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Site from './pages/index';
 
+import '../style/style.scss';
+
 ReactDOM.render(<Site />, window.document.getElementById('react-root'));
